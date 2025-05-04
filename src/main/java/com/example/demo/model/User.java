4450f4bc1bcf38a7f922efcebfb482422d8337d8
jarodri1 <j.rodriguez48@yahoo.com>
2025-05-04 12:39:01 -0500

    Update configuration files to enhance security and fix typo
    Replaced hardcoded email password with a placeholder in `application.properties` for better security. Corrected a syntax error in the `application-prod.yml` database URL to ensure proper environment variable resolution.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @Entity
 @Table(name = "users")
 public class User {
